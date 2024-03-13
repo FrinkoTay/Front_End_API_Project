05e87bcec002d1e4b971bd75d3419535c1888210
FrinkoTay <francis.taylor20027@gmail.com>
2024-03-13 09:28:43 +0000

    add Loading message for Articles.jsx

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -4,16 +4,20 @@ import ArticleCard from "./ArticleCard"
 
 const Articles = () => {
     const [articles, setArticles] = useState([])
+    const [isLoading, setIsLoading] = useState('')
 
     useEffect(() => {
+        setIsLoading('Loading Articles...')
         getArticles()
         .then((fetchedArticles) => {
             setArticles(fetchedArticles)
+            setIsLoading('')
         })
     }, [])
 
     return <header>
         <h2> Articles: </h2>
+        <h1> {isLoading} </h1>
         <div>
             {articles.map((article) => {
                 return <ArticleCard key={article.article_id} article={article}/>
