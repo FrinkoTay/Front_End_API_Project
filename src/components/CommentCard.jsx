f5e24b59e2d3a76c4054866cf03bb24a93c0cf8d
FrinkoTay <francis.taylor20027@gmail.com>
2024-03-14 10:28:56 +0000

    add delete comment button to user's comments

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,8 +1,17 @@
+import UserContext from '../contexts/User';
+import { useContext } from "react"
+
 const CommentCard = ({comment}) => {
+    const userLoggedIn = useContext(UserContext)
+
     return <div className="comment-card">
         <h4> {comment.author} </h4>
         <p> {comment.body} </p>
         <p> {comment.created_at} </p>
+        <div> 
+            {userLoggedIn === comment.author ?
+            <button> Delete Comment </button> : ""} 
+        </div>
     </div>
 }
 
