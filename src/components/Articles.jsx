b1992a54adce56602e830b88cea19552072e7066
FrinkoTay <francis.taylor20027@gmail.com>
2024-03-14 13:41:52 +0000

    add functionality to sort articles by categories

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,22 +1,41 @@
-import { getArticles } from "../api"
+import { getArticles, getTopics } from "../api"
 import { useState, useEffect } from "react"
 import ArticleCard from "./ArticleCard"
+import { useSearchParams, Link } from 'react-router-dom';
 
 const Articles = () => {
     const [articles, setArticles] = useState([])
+    const [categories, setCategories] = useState([])
     const [isLoading, setIsLoading] = useState('')
+    const searchParams = useSearchParams()[0]
+    const categoryName = searchParams.get('category')
+
+    useEffect(() => {
+        getTopics()
+        .then((fetchedCategories) => {
+            setCategories(fetchedCategories)
+        })
+    }, [])
 
     useEffect(() => {
         setIsLoading('Loading Articles...')
-        getArticles()
+        getArticles(categoryName)
         .then((fetchedArticles) => {
             setArticles(fetchedArticles)
             setIsLoading('')
         })
-    }, [])
+    }, [categoryName])
 
     return <header>
         <h2> Articles: </h2>
+        <h3>Categories</h3>
+        <nav>
+            {categories.map((category) => {
+                return <Link key={category.description} 
+                    to={`/articles?category=${category.slug}`}> 
+                    {category.slug} </Link>
+            })}
+        </nav>
         <h1> {isLoading} </h1>
         <div>
             {articles.map((article) => {
