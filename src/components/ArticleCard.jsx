e8c88aaa24bd7e80369a4d8c0d0f928e29503878
FrinkoTay <francis.taylor20027@gmail.com>
2024-03-17 23:25:17 +0000

    add votes and date to article cards

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -6,6 +6,8 @@ const ArticleCard = ({article}) => {
         <p>Author: {article.author} </p>
         <p>Topic: {article.topic} </p>
         <p>Comments: {article.comment_count} </p>
+        <p>Votes: {article.votes}</p>
+        <p>Date: {article.created_at}</p>
         <img className="article-img" src={ article.article_img_url }></img>
         </Link>
         {/*<Link to={`/${article.article_id}`}> See more... </Link>*/}
